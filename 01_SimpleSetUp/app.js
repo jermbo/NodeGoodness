c626f26f1147dad9df6d4e3a7f077287e7eeb7a7
jermbo <jermbo@jermbos-MacBook-Pro.local>
2015-05-27 20:14:56 -0400

    initial set up and first example.

---
+++ 01_SimpleSetUp/app.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+var app = http.createServer(function (req, res, next) {
+  res.writeHead(200, {'Content-Type' : 'text/html'});
+  console.log('listening');
+  res.end('<h1>Hello World</h1>');
+});
+
+app.listen(3000, 'localhost');
+
+console.log('server app running on port 3000');
